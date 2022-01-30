f73a00198b2837d90a10b9355348d27ca1b1af72
mojo9492 <mojo9492@outlook.com>
2022-01-30 14:49:35 -0800

    add mongodb_test.py

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.qyihp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+client = MongoClient(url)
+
+db = client.pytech
+
+print(db.list_collection_names())
